abb108c04ca32c5b386a1a0e6b3456790490b129
Marc-Philipp Esser <m-esser@mail.de>
2023-09-08 23:40:00 +0200

    Check available python modules as well

--- src/prefect/healthcheck.py
+++ src/prefect/healthcheck.py
@@ -11,6 +11,9 @@ def healthcheck():
     logger = get_run_logger()
     logger.info(f"Running on Network '{node()}' and Instance '{platform()}'")
 
+    logger.info("The following modules are available in this flow:")
+    help("modules")
+
 
 if __name__ == "__main__":
     healthcheck()
