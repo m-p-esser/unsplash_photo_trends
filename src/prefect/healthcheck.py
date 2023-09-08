9126b860b2bb2cddcf0ee7034964b9d3351f87ee
Marc-Philipp Esser <m-esser@mail.de>
2023-09-08 23:57:29 +0200

    list available packages using importlib.metadata

--- src/prefect/healthcheck.py
+++ src/prefect/healthcheck.py
@@ -1,6 +1,7 @@
 """Flow to check health of a machine """
 
-from platform import node, platform
+from importlib.metadata import distributions
+from platform import node, platform, python_version
 
 from prefect import flow, get_run_logger
 
@@ -10,9 +11,11 @@ def healthcheck():
     """Collect information about machine and if it is accessible"""
     logger = get_run_logger()
     logger.info(f"Running on Network '{node()}' and Instance '{platform()}'")
+    logger.info(f"Running on Python Version '{python_version()}'")
 
     logger.info("The following modules are available in this flow:")
-    logger.info(help("modules"))
+    for dist in distributions():
+        logger.info(f"{dist.metadata['Name']}=={dist.version}")
 
 
 if __name__ == "__main__":
