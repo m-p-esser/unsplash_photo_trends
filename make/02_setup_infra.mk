44729e86ec53331ed15402c521094225aa0ee049
Marc-Philipp Esser <m-esser@mail.de>
2023-09-09 18:50:35 +0200

    Add cleaning functionality for GCS buckets

--- make/02_setup_infra.mk
+++ make/02_setup_infra.mk
@@ -5,3 +5,9 @@ create-gcs-buckets: ### Create Google Cloud Storage Buckets
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-topics-dev
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-topics-test
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-topics-prod
+
+.PHONY: cleanup-gcs-buckets
+cleanup-gcs-buckets: ### Delete all Contents from Google Cloud Storage Buckets
+	gsutil rm gs://unsplash-topics-dev/**
+	gsutil rm gs://unsplash-topics-test/**
+	gsutil rm gs://unsplash-topics-prod/**
