b859dab6d8f7cf0ec658a584fcdb825e9a6e7ad7
Marc-Philipp Esser <m-esser@mail.de>
2023-09-09 13:05:43 +0200

    remove spark references

--- make/02_setup_infra.mk
+++ make/02_setup_infra.mk
@@ -1,23 +1,7 @@
 ##@ [Infrastructure: Setup]
 
-# .PHONY: create-environment-yaml
-# create-environment-yaml: ### Create environment.yaml using pyproject.toml as base
-# 	poetry export -o requirements.txt --without-hashes --without-urls --without=dev,test
-# 	python3 src/scripts/requirements_to_conda_env.py
-# 	gsutil rm -r gs://${SPARK_STAGING_BUCKET_NAME}
-# 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://${SPARK_STAGING_BUCKET_NAME}
-# 	gsutil cp environment.yaml gs://${SPARK_STAGING_BUCKET_NAME}/environment.yaml
-
 .PHONY: create-gcs-buckets
 create-gcs-buckets: ### Create Google Cloud Storage Buckets
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-topics-dev
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-topics-test
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-topics-prod
-
-# .PHONY: create-spark-cluster 
-# create-spark-cluster: ## Create GCP Dataproc (Spark) Cluster
-# 	gcloud dataproc clusters create ${SPARK_CLUSTER_NAME} \
-# 	--region=northamerica-northeast2 \
-# 	--service-account=$(GCP_DEPLOYMENT_SERVICE_ACCOUNT)@$(GCP_PROJECT_ID).iam.gserviceaccount.com \
-# 	--image-version=2.0 \
-# 	--bucket=${SPARK_STAGING_BUCKET_NAME}
