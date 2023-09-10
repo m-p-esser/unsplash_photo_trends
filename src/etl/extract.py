2ad5d24d2f0f9600f422cd3a35b1b4b9d5a51099
Marc-Philipp Esser <m-esser@mail.de>
2023-09-10 09:23:56 +0200

    remove unnecessary comments

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -17,14 +17,6 @@ def download_blob_to_file(
     gcp_credential_block_name: str,
 ) -> storage.bucket.Bucket.blob:
     """Downloads a blob from the bucket."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-
-    # The ID of your GCS object
-    # source_blob_name = "storage-object-name"
-
-    # The path to which the file should be downloaded
-    # destination_file_name = "local/path/to/file"
 
     gcp_credentials = GcpCredentials.load(gcp_credential_block_name)
     storage_client = gcp_credentials.get_cloud_storage_client()
@@ -38,12 +30,6 @@ def download_blob_to_file(
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(destination_file_name)
 
-    # print(
-    #     "Downloaded storage object {} from bucket {} to local file {}.".format(
-    #         source_blob_name, bucket_name, destination_file_name
-    #     )
-    # )
-
     return blob
 
 
@@ -51,11 +37,6 @@ def download_blob_into_memory(
     bucket_name: str, blob_name: str, gcp_credential_block_name: str
 ) -> str:
     """Downloads a blob into memory."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-
-    # The ID of your GCS object
-    # blob_name = "storage-object-name"
 
     gcp_credentials = GcpCredentials.load(gcp_credential_block_name)
     storage_client = gcp_credentials.get_cloud_storage_client()
@@ -69,11 +50,6 @@ def download_blob_into_memory(
     blob = bucket.blob(blob_name)
     contents = blob.download_as_string()
 
-    # print(
-    #     "Downloaded storage object {} from bucket {} as the following string: {}.".format(
-    #         blob_name, bucket_name, contents
-    #     )
-    # )
     return contents
 
 
