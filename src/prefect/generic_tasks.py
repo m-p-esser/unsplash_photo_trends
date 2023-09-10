980c6b64457aa6ca9d0fec70f937771600864e9a
Marc-Philipp Esser <m-esser@mail.de>
2023-09-10 10:43:41 +0200

    add generic tasks that can be reused across flows

---
+++ src/prefect/generic_tasks.py
@@ -0,0 +1,98 @@
+""" Collection of generic tasks that can be reused across flows """
+
+import datetime
+import json
+from tempfile import NamedTemporaryFile
+
+import pandas as pd
+import requests
+from google.cloud import storage
+
+from prefect import get_run_logger, task
+from prefect.blocks.system import Secret
+from src.etl.load import upload_blob_from_file
+from src.utils import timer
+
+
+@task(retries=3, retry_delay_seconds=10)
+@timer
+def request_unsplash(endpoint: str) -> list[dict]:
+    """Request data from Unsplash API"""
+    logger = get_run_logger()
+
+    UNSPLASH_ACCESS_KEY = Secret.load("unsplash-photo-trends-unsplash-access-key").get()
+    BASE_URL = "https://api.unsplash.com"
+    URI = BASE_URL + endpoint
+
+    logger.info(f"Requesting endpoint: {URI}")
+    response = requests.get(
+        url=URI, params={"client_id": UNSPLASH_ACCESS_KEY, "per_page": 30}
+    )
+
+    response.raise_for_status()
+
+    rate_limit_limit = int(response.headers["X-Ratelimit-Limit"])
+    rate_limit_remaining = int(response.headers["X-Ratelimit-Remaining"])
+    consumed_quota = (rate_limit_limit - rate_limit_remaining) / rate_limit_limit
+
+    if consumed_quota > 0.8:
+        logger.warning(
+            f"Rate limit almost reached: {consumed_quota}%% of Quota consumed."
+        )
+
+    if rate_limit_remaining == 0:
+        logger.error(
+            f"Rate limit reached: {consumed_quota}%% of Quota consumed. Wait to continue"
+        )
+
+    response_json = response.json()
+    logger.info(f"Response contains data for {len(response_json)} topics")
+
+    return response_json
+
+
+@task(retries=3, retry_delay_seconds=10)
+@timer
+def response_data_to_df(response_json: dict, response_data_name: str) -> pd.DataFrame:
+    """Store Response data as in Dataframe"""
+    logger = get_run_logger()
+
+    with NamedTemporaryFile(delete=True, suffix=".jsonl") as temp_jsonl:
+        logger.info(f"Writing {response_data_name} data to file {temp_jsonl.name}")
+
+        for item in response_json:
+            item_string = json.dumps(item) + "\n"
+            temp_jsonl.write(item_string.encode())
+
+        df = pd.read_json(temp_jsonl.name, lines=True)
+        df["requested_data_at"] = datetime.datetime.now()
+        logger.info(
+            f"The Dataframe contains {df.shape[1]} columns and {df.shape[0]} rows"
+        )
+
+    return df
+
+
+@task(retries=3, retry_delay_seconds=10)
+@timer
+def store_response_df_to_gcs_bucket(
+    df: pd.DataFrame, response_data_name: str, env: str = "dev"
+) -> storage.blob.Blob:
+    """Store Dataframe as Blob in Google Cloud Storage Bucket"""
+    logger = get_run_logger()
+
+    with NamedTemporaryFile(delete=True, suffix=".parquet") as temp_parquet:
+        df.to_parquet(temp_parquet.name)
+
+        today = datetime.date.today().strftime("%Y%m%d")
+        blob_name = f"topics_{today}.parquet"
+        blob = upload_blob_from_file(
+            bucket_name=f"unsplash-{response_data_name}-{env}",
+            source_file_name=temp_parquet.name,
+            destination_blob_name=blob_name,
+            gcp_credential_block_name="unsplash-photo-trends-deployment-sa",
+        )
+
+        logger.info(f"Uploaded topics data to {blob}")
+
+        return blob
