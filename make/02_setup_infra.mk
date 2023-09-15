3abc01a95eb0dd8673a682c340aad5d95209b5c7
Marc-Philipp Esser <m-esser@mail.de>
2023-09-16 00:29:33 +0200

    add more buckets

--- make/02_setup_infra.mk
+++ make/02_setup_infra.mk
@@ -10,6 +10,9 @@ create-gcs-buckets: ### Create Google Cloud Storage Buckets
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-monthly-platform-stats-prod || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-unit-tests-dev || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-unit-tests-test || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-metadata-dev || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-metadata-test || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-metadata-prod || true
 
 .PHONY: cleanup-gcs-buckets
 cleanup-gcs-buckets: ### Delete all Contents from Google Cloud Storage Buckets
