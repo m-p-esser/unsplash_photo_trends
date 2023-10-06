19e8853c25e181ab8515f7f3ef2f781f2dc11e39
Marc-Philipp Esser <m-esser@mail.de>
2023-10-06 22:49:34 +0200

    add prefect block for bright data and residential proxies

--- src/blocks/create_bright_data_secrets.py
+++ src/blocks/create_bright_data_secrets.py
@@ -5,6 +5,8 @@ from src.utils import load_env_variables
 
 env_vars = load_env_variables()
 
+#### Residential Proxy ######
+
 PREFECT_BLOCK_NAME_BRIGHT_DATA_RESIDENTIAL_PROXY = (
     f"{env_vars['GCP_PROJECT_ID']}-bright-data-residential-proxy-username"
 )
@@ -20,3 +22,21 @@ PREFECT_BLOCK_NAME_BRIGHT_DATA_RESIDENTIAL_PROXY = (
 gcp_credentials_block = Secret(
     value=env_vars["BRIGHT_DATA_RESIDENTAL_PROXY_PASSWORD"]
 ).save(name=PREFECT_BLOCK_NAME_BRIGHT_DATA_RESIDENTIAL_PROXY, overwrite=True)
+
+#### Datacenter Proxy ######
+
+PREFECT_BLOCK_NAME_BRIGHT_DATA_DATACENTER_PROXY = (
+    f"{env_vars['GCP_PROJECT_ID']}-bright-data-datacenter-proxy-username"
+)
+
+gcp_credentials_block = Secret(
+    value=env_vars["BRIGHT_DATA_DATACENTER_PROXY_USERNAME"]
+).save(name=PREFECT_BLOCK_NAME_BRIGHT_DATA_DATACENTER_PROXY, overwrite=True)
+
+PREFECT_BLOCK_NAME_BRIGHT_DATA_DATACENTER_PROXY = (
+    f"{env_vars['GCP_PROJECT_ID']}-bright-data-datacenter-proxy-password"
+)
+
+gcp_credentials_block = Secret(
+    value=env_vars["BRIGHT_DATA_DATACENTER_PROXY_PASSWORD"]
+).save(name=PREFECT_BLOCK_NAME_BRIGHT_DATA_DATACENTER_PROXY, overwrite=True)
