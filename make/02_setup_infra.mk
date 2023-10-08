a1099e9cf9afe052d338cd775035ce9528fdc53f
Marc-Philipp Esser <m-esser@mail.de>
2023-10-08 12:47:45 +0200

    add buckets for task/flow runs

--- make/02_setup_infra.mk
+++ make/02_setup_infra.mk
@@ -8,8 +8,6 @@ create-gcs-buckets: ### Create Google Cloud Storage Buckets
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-monthly-platform-stats-dev || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-monthly-platform-stats-test || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-monthly-platform-stats-prod || true
-	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-unit-tests-dev || true
-	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://unsplash-unit-tests-test || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-metadata-dev || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-metadata-test || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-metadata-prod || true
@@ -17,6 +15,12 @@ create-gcs-buckets: ### Create Google Cloud Storage Buckets
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-dev || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-test || true
 	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://photos-editorial-prod || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://task-runs-dev || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://task-runs-test || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://task-runs-prod || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://task-runs-dev || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://task-runs-test || true
+	gsutil mb -c standard -l ${GCP_DEFAULT_REGION} gs://task-runs-prod || true
 
 .PHONY: cleanup-gcs-buckets
 cleanup-gcs-buckets: ### Delete all Contents from Google Cloud Storage Buckets
