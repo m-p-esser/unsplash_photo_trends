b83e68329897d7118ed402a6664b38a9b20e3e38
Marc-Philipp Esser <m-esser@mail.de>
2023-10-08 17:52:54 +0200

    cache blob

--- src/prefect/generic_tasks.py
+++ src/prefect/generic_tasks.py
@@ -14,6 +14,7 @@ from google.cloud import storage
 
 from prefect import get_run_logger, task
 from prefect.blocks.system import Secret
+from prefect.tasks import task_input_hash
 from src.etl.load import upload_blob_from_file, upload_blob_from_memory
 
 
@@ -156,9 +157,8 @@ async def request_unsplash_api_async(
 
 
 @task(
-    # cache_key_fn=task_input_hash,
-    # cache_expiration=datetime.timedelta(hours=1),
-    timeout_seconds=20,
+    cache_key_fn=task_input_hash,
+    cache_expiration=datetime.timedelta(hours=1),
 )
 def upload_file_to_gcs_bucket(
     gcp_credential_block_name: str,
