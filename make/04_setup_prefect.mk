80cd4792171f5bb5bf3ff4bb929465883674d40a
Marc-Philipp Esser <m-esser@mail.de>
2023-10-13 21:10:05 +0200

    add compute engine as prefect agent

--- make/04_setup_prefect.mk
+++ make/04_setup_prefect.mk
@@ -14,6 +14,7 @@ create-prefect-blocks: ## Create Prefect Blocks
 	prefect block register --file src/blocks/create_bigquery.py
 	prefect block register --file src/blocks/create_zenrows_api_key.py
 	prefect block register --file src/blocks/create_bright_data_secrets.py
+	prefect block register --file src/blocks/create_vertex_ai.py
 
 .PHONY: create-prefect-artifact-repository
 create-prefect-artifact-repository: ## Create GCP Artificat Repository for Prefect Flows
@@ -29,6 +30,24 @@ create-google-cloud-run-push-work-pool: ## Create Google Cloud Run Workflow (for
 	prefect work-pool create $(ENV)-cloud-run-push-work-pool \
 		--type cloud-run:push
 # Need to manually add GCP Credentials under Work Pool Settings
+
+.PHONY: setup-compute-engine
+setup-compute-engine: ## Create a GCP VM which triggers Flow runs 
+	gcloud compute instances create-with-container prefect-${ENV}-vm \
+			--container-image $(GCP_DEFAULT_REGION)-docker.pkg.dev/$(GCP_PROJECT_ID)/prefect-${ENV}/prefect:$(PREFECT_VERSION)-python$(PYTHON_VERSION) \
+			--container-mount-host-path=host-path=/var/run/docker.sock,mount-path=/var/run/docker.sock,mode=rw \
+			--container-privileged \
+			--container-env PREFECT_API_URL=${PREFECT_API_URL} \
+			--container-env PREFECT_API_KEY=${PREFECT_API_KEY} \
+			--container-command="prefect" \
+			--container-arg="worker" \
+			--container-arg="start" \
+			--container-arg="--pool" \
+			--container-arg="${ENV}-vertex-ai-work-pool" \
+			--container-restart-policy='always' \
+			--boot-disk-size="200Gi" \
+			--machine-type="e2-micro" \
+			--zone=$(GCP_DEFAULT_ZONE)
 
 .PHONY: setup-prefect
 setup-prefect: ## Setup Prefect Blocks, Artifact Repo and Work Pools	
